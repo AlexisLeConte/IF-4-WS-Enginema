630146b9e41885ca577e0640140b99f17112e519
Thomas Malvoisin <thomas.malvoisin@hotmail.fr>
2018-11-20 01:33:16 +0100

    Begin of resource information view

--- project/src/main/webapp/scripts/driver.js
+++ project/src/main/webapp/scripts/driver.js
@@ -1,128 +1,211 @@
-$(document).ready(function() {
-  // Initialization
-  initGraph("#viewport");
-  loadSuggestions();
-  
-  // Bind events to actions
-  $("#search-button").on("click", function() {
-    var query = $("#search-bar").val();
-      if (query) {
-        queryByName(query);
-      } else {
-        loadSuggestions();
-      }
-  });
-  
-  $("#search-bar").on("keyup", function(e) {
-    e.preventDefault();
-    if (e.keyCode === 13) {
-      var query = $("#search-bar").val();
-      if (query) {
-        queryByName(query);
-      } else {
-        loadSuggestions();
-      }
-    }
-  });
+$(document).ready(function () {
+// Initialization
+    initGraph("#viewport");
+    loadSuggestions();
+    // Bind events to actions
+    $("#search-button").on("click", function () {
+        var query = $("#search-bar").val();
+        if (query) {
+            queryByName(query);
+        } else {
+            loadSuggestions();
+        }
+    });
+    $("#search-bar").on("keyup", function (e) {
+        e.preventDefault();
+        if (e.keyCode === 13) {
+            var query = $("#search-bar").val();
+            if (query) {
+                queryByName(query);
+            } else {
+                loadSuggestions();
+            }
+        }
+    });
 });
-
 function loadSuggestions() {
-  clearGraph();
-  $.ajax({
-    url: "./ActionServlet",
-    method: "GET",
-    data: {
-      action: "loadSuggestions"
-    },
-    dataType: "json"
-  }).done(function(data) {
-    $("#query-results").html("");
-    if (data.responseType === "suggestions") {
-      appendTitle($("#query-results"), "Search suggestions");
-      for (var i=0; i<data.responseContent.length; i++) {
-        appendQuerySuggestion($("#query-results"), data.responseContent[i]);
-      }
-    } else {
-      appendErrorMessage($("#query-results"), "Service unavailable");
-    }
-  });
+    clearGraph();
+    $.ajax({
+        url: "./ActionServlet",
+        method: "GET",
+        data: {
+            action: "loadSuggestions"
+        },
+        dataType: "json"
+    }).done(function (data) {
+        $("#query-results").html("");
+        if (data.responseType === "suggestions") {
+            appendTitle($("#query-results"), "Search suggestions");
+            for (var i = 0; i < data.responseContent.length; i++) {
+                appendQuerySuggestion($("#query-results"), data.responseContent[i]);
+            }
+        } else {
+            appendErrorMessage($("#query-results"), "Service unavailable");
+        }
+    });
 }
 
 function queryByName(name) {
-  $.ajax({
-    url: "./ActionServlet",
-    method: "GET",
-    data: {
-      action: "queryByName",
-      query: name
-    },
-    dataType: "json"
-  }).done(function (data) {
-    $("#query-results").html("");
-    clearGraph();
-    if (data.responseType === "queryResults") {
-      appendTitle($("#query-results"), "Search results");
-      var numberOfFilms = 0;
-      var numberOfCompanies = 0;
-      var numberOfPersons = 0;
-      var numberOfResults = data.responseContent.length;
-      for (var i=0; i<data.responseContent.length; i++) {
-        var result = data.responseContent[i];
-        appendQuerySuggestion($("#query-results"), result);
-        if (result.resourceType === "film") {
-          numberOfFilms++;
-        }
-        if (result.resourceType === "company") {
-          numberOfCompanies++;
-        }
-        if (result.resourceType === "person") {
-          numberOfPersons++;
-        }
-      }
-      var resultAnalysis = {
-        nodes: {
-          nresults: {name: "Results (" + numberOfResults + ")", color: "#171a1d", uri: "", radius: 0},
-          nfilms: {name: "Films (" + numberOfFilms + ")", color: "#004085", uri: "", radius: 0},
-          ncompanies: {name: "Companies (" + numberOfCompanies +")", color: "#155724", uri: "", radius: 0},
-          npersons: {name: "Persons (" + numberOfPersons + ")", color: "#822224", uri: "", radius: 0}
+    $.ajax({
+        url: "./ActionServlet",
+        method: "GET",
+        data: {
+            action: "queryByName",
+            query: name
         },
-        edges: {
-          nresults: {
-            nfilms: {type: ""},
-            ncompanies: {type: ""},
-            npersons: {type: ""}
-          }
+        dataType: "json"
+    }).done(function (data) {
+        $("#query-results").html("");
+        clearGraph();
+        if (data.responseType === "queryResults") {
+            appendTitle($("#query-results"), "Search results");
+            var numberOfFilms = 0;
+            var numberOfCompanies = 0;
+            var numberOfPersons = 0;
+            var numberOfResults = data.responseContent.length;
+            for (var i = 0; i < data.responseContent.length; i++) {
+                var result = data.responseContent[i];
+                appendQuerySuggestion($("#query-results"), result);
+                if (result.resourceType === "film") {
+                    numberOfFilms++;
+                }
+                if (result.resourceType === "company") {
+                    numberOfCompanies++;
+                }
+                if (result.resourceType === "person") {
+                    numberOfPersons++;
+                }
+            }
+            var resultAnalysis = {
+                nodes: {
+                    nresults: {name: "Results (" + numberOfResults + ")", color: "#171a1d", uri: "", radius: 0},
+                    nfilms: {name: "Films (" + numberOfFilms + ")", color: "#004085", uri: "", radius: 0},
+                    ncompanies: {name: "Companies (" + numberOfCompanies + ")", color: "#155724", uri: "", radius: 0},
+                    npersons: {name: "Persons (" + numberOfPersons + ")", color: "#822224", uri: "", radius: 0}
+                },
+                edges: {
+                    nresults: {
+                        nfilms: {type: ""},
+                        ncompanies: {type: ""},
+                        npersons: {type: ""}
+                    }
+                }
+            };
+            updateGraph(resultAnalysis);
+        } else if (data.responseType === "noResult") {
+            appendErrorMessage($("#query-results"), "Sorry, no results found :(");
+        } else {
+            appendErrorMessage($("#query-results"), "Service unavailable");
         }
-      };
-      updateGraph(resultAnalysis);
-    } else if (data.responseType === "noResult") {
-      appendErrorMessage($("#query-results"), "Sorry, no results found :(");
-    } else {
-      appendErrorMessage($("#query-results"), "Service unavailable");
-    }
-  });
+    });
 }
 
 function queryByUri(uri) {
-  
-  alert("URI request : " + uri);
-  
-  $.ajax({
-    url: "./ActionServlet",
-    method: "GET",
-    data: {
-      action: "queryByUri",
-      query: uri
-    },
-    dataType: "json"
-  }).done(function (data) {
+
+    let test = {
+        "resourceInfo": [
+            {
+                "type": "name",
+                "value": "Harry Potter (film series)"
+            },
+            {
+                "type": "Budget",
+                "value": "1.155E9"
+            },
+            {
+                "type": "Box Office",
+                "value": "7.723E9"
+            },
+            {
+                "type": "Run Time",
+                "value": "70740.0"
+            },
+            {
+                "name": "Starring",
+                "value": [
+                    {
+                        "value": "Daniel Radcliffe",
+                        "uri": "http://dbpedia.org/resource/Daniel_Radcliffe"
+                    },
+                    {
+                        "value": "Emma Watson",
+                        "uri": "http://dbpedia.org/resource/Emma_Watson"
+                    },
+                    {
+                        "value": "Rupert Grint",
+                        "uri": "http://dbpedia.org/resource/Rupert_Grint"
+                    }
+                ]
+            },
+            {
+                "name": "Directed By",
+                "value": [
+                    {
+                        "value": "David Yates",
+                        "uri": "http://dbpedia.org/resource/David_Yates"
+                    },
+                    {
+                        "value": "Alfonso Cuarón",
+                        "uri": "http://dbpedia.org/resource/Alfonso_Cuarón"
+                    },
+                    {
+                        "value": "Mike Newell",
+                        "uri": "http://dbpedia.org/resource/Mike_Newell_(director)"
+                    },
+                    {
+                        "value": "Chris Columbus",
+                        "uri": "http://dbpedia.org/resource/Chris_Columbus_(filmmaker)"
+                    }
+                ]
+            },
+            {
+                "name": "Music By",
+                "value": [
+                    {
+                        "value": "Alexandre Desplat",
+                        "uri": "http://dbpedia.org/resource/Alexandre_Desplat"
+                    },
+                    {
+                        "value": "Patrick Doyle",
+                        "uri": "http://dbpedia.org/resource/Patrick_Doyle"
+                    },
+                    {
+                        "value": "Nicholas Hooper",
+                        "uri": "http://dbpedia.org/resource/Nicholas_Hooper"
+                    },
+                    {
+                        "value": "John Williams",
+                        "uri": "http://dbpedia.org/resource/John_Williams"
+                    }
+                ]
+            }
+        ]};
+
     $("#query-results").html("");
-    clearGraph();
-    if (data.responseType === "resourceInfoGraph") {
-      updateGraph(data.responseContent.resourceGraph);
-      appendResourceInformation($("#query-results"), data.responseContent.resourceInfo);
-    } else {
-      appendErrorMessage($("#query-results"), "Service unavailable :(");
-    }
-  });
+    appendResourceInformation($("#query-results"), test.resourceInfo);
+
+
+//alert(JSON.stringify(film));
+
+//  alert("URI request : " + uri);
+
+//  $.ajax({
+//    url: "./ActionServlet",
+//    method: "GET",
+//    data: {
+//      action: "queryByUri",
+//      query: uri
+//    },
+//    dataType: "json"
+//  }).done(function (data) {
+//      
+//    $("#query-results").html("");
+//    clearGraph();
+//    if (data.responseType === "resourceInfoGraph") {
+//      updateGraph(data.responseContent.resourceGraph);
+//      appendResourceInformation($("#query-results"), data.responseContent.resourceInfo);
+//    } else {
+//      appendErrorMessage($("#query-results"), "Service unavailable :(");
+//    }
+//  });
 }
--- project/src/main/webapp/scripts/htmlfactory.js
+++ project/src/main/webapp/scripts/htmlfactory.js
@@ -1,30 +1,54 @@
 function appendErrorMessage(component, message) {
-  component.append("<div class='row-sm-2 side-title'>" + message + "</div>");
+    component.append("<div class='row-sm-2 side-title'>" + message + "</div>");
 }
 
 function appendTitle(component, message) {
-  component.append("<div class='row-sm-2 side-title'>" + message + "</div>");
+    component.append("<div class='row-sm-2 side-title'>" + message + "</div>");
 }
 
 function appendQuerySuggestion(component, suggestion) {
-  var suggestionClass;
-  if (suggestion.resourceType === "company") {
-    suggestionClass = "company-suggestion";
-  }
-  if (suggestion.resourceType === "film") {
-    suggestionClass = "film-suggestion";
-  }
-  if (suggestion.resourceType === "person") {
-    suggestionClass = "person-suggestion";
-  }
-  var onClickAction = "queryByUri('" + suggestion.resourceUri + "')";
-  component.append("<div class='row-sm-2 search-suggestion " + suggestionClass + "' onclick=\"" + onClickAction + "\">" + suggestion.resourceName + "</div>");
+    var suggestionClass;
+    if (suggestion.resourceType === "company") {
+        suggestionClass = "company-suggestion";
+    }
+    if (suggestion.resourceType === "film") {
+        suggestionClass = "film-suggestion";
+    }
+    if (suggestion.resourceType === "person") {
+        suggestionClass = "person-suggestion";
+    }
+    var onClickAction = "queryByUri('" + suggestion.resourceUri + "')";
+    component.append("<div class='row-sm-2 search-suggestion " + suggestionClass + "' onclick=\"" + onClickAction + "\">" + suggestion.resourceName + "</div>");
 }
 
 function appendResourceInformation(component, information) {
-  
+    appendFilmInformation(component, information);
+    appendFilmStarring(component, information);
+}
+
+function appendFilmInformation(component, information) {
+    component.append("<div class='card'><div class='card-body'><div class='row-sm-2 search-suggestion film-suggestion text-center'>" + information[0].value + "</div></div>"
+            + "<ul class='list-group list-group-flush'>"
+            + "<li class='list-group-item no-border film-info-item'><span style='font-weight:bold'>" + information[1].type + "</span>" + information[1].value + " USD </li>"
+            + "<li class='list-group-item no-border film-info-item'><span style='font-weight:bold'>" + information[2].type + "</span>" + information[2].value + " USD </li>"
+            + "<li class='list-group-item no-border film-info-item'><span style='font-weight:bold'>" + information[3].type + "</span>" + information[3].value + " s </li>"
+            + "</ul></div>");
+}
+
+function appendFilmStarring(component, information) {
+    let starringCard = "<div class='card my-2'><div class='card-body'><h5 class='card-title'>" + "Starring" + "</h5></div>";
+    starringCard += "<ul class='list-group list-group-flush'>"
+    
+    information[4].value.map((actor) => {
+        starringCard += "<li class='list-group-item no-border'><div class='row-sm-2 search-suggestion person-suggestion text-center'>" + actor.value + "</div></li>";
+//        console.log(actor);
+        return actor;
+    });
+    
+    starringCard += "</ul></div>";
+    component.append(starringCard);
 }
 
 function appendInformation(component, title, content) {
-  component.append("<div class='row-sm-2'>" + title + " : " + content + "</div>");
+    component.append("<div class='row-sm-2'>" + title + " : " + content + "</div>");
 }
